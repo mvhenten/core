e05f2bf961ea4293693b26506fd6224c25888f22
mvhenten <mvhenten@amazon.com>
2018-03-28 09:53:11 +0000

    use the setup_paths from @c9

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ var os = require("os");
 var urls = require("c9/urls");
 var hostname = require("c9/hostname");
 var child_process = require("child_process");
-require("setup_paths");
+require("@c9/setup_paths");
 
 if (process.version.match(/^v0/) && parseFloat(process.version.substr(3)) < 10) {
     console.warn("You're using Node.js version " + process.version
